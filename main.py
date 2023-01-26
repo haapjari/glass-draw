7f98ef9fc60b75a4e141bb39207e83d057436e76
Jari Haapasaari <jari.haapasaari@nokia.com>
2023-01-26 20:06:36 +0200

    quality measure calculation and .csv generation

--- main.py
+++ main.py
@@ -2,39 +2,52 @@ from datetime import datetime
 import csv
 import chardet
 import sys
+import io
+import pandas as pd
+from collections import OrderedDict
 
 def calculate_quality_measure_date(values):
-    # convert all date strings to datetime objects
-    datetime_values = [datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ') for value in values]
-    # Find the oldest date in the list
-    oldest_date = min(datetime_values)
-    # Find the newest date in the list
-    newest_date = max(datetime_values)
-    # Count how many values in the list are older than newest_date
-    below_newest_count = len([value for value in datetime_values if value < newest_date])
-    # Calculate the quality measure by dividing the count of older than newest values by the length of the list
-    quality_measure = below_newest_count / len(values) * 5
-    # return the quality measure
+    datetime_values = []
+    for value in values:
+        try:
+            if isinstance(value, (float, int)):
+                # Handle cases where value is a float or int
+                value = str(value)
+            datetime_value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+            datetime_values.append(datetime_value)
+        except (ValueError, TypeError):
+            # value is not a valid datetime string, so we will skip it
+            pass
+
+    if len(datetime_values) > 0:
+        oldest_date = min(datetime_values)
+        newest_date = max(datetime_values)
+        quality_measures = [(date - oldest_date).days / (newest_date - oldest_date).days * 5 for date in datetime_values]
+        return quality_measures
+    else:
+        return []
+
+
+def quality_measure(values, weight=1):
+    lowest_value, highest_value = min(values), max(values)
+    quality_measure = [(value - lowest_value) / (highest_value - lowest_value) * 5 * weight for value in values]
     return quality_measure
 
-def calculate_quality_measure(values):
-    # Find the lowest value in the list
-    lowest_value = min(values)
 
-    # Find the highest value in the list
-    highest_value = max(values)
-    
-    # Calculate the middle point between the lowest and highest value
-    middle_point = (lowest_value + highest_value) / 2
-    
-    # Count how many values in the list are less than middle_point
-    below_middle_count = len([value for value in values if value < middle_point])
-    
-    # Calculate the quality measure by dividing the count of below middle values by the length of the list
-    quality_measure = below_middle_count / len(values) * 5
-    
-    # return the quality measure
-    return quality_measure
+def calculate_quality_measure(file_name):
+    # Read the CSV file
+    df = pd.read_csv(file_name)
+
+    # Calculate the average for each row
+    df["quality_measure"] = df.mean(numeric_only=True, axis=1)
+
+    # Save the DataFrame to a new CSV file
+    df.to_csv("new_"+file_name, index=False)
+
+    # Create a dictionary from the DataFrame
+    qm = df.set_index("repository_url")["quality_measure"].to_dict()
+
+    return qm
 
 
 def read_csv_file(file_path):
@@ -47,10 +60,54 @@ def read_csv_file(file_path):
             data += ",".join(row) + "\n"
     return data
 
+
+def extract_columns(csv_string: str, columns: list):
+    # Create a DataFrame from the CSV string
+    df = pd.read_csv(io.StringIO(csv_string))
+    extracted_columns = {}
+    for column in columns:
+        extracted_columns[column] = df[column]
+    return extracted_columns
+
+
 def main():
     file_name = " ".join(sys.argv[1:])
-
     data = read_csv_file(file_name)
-    print(data)
+
+    columns = extract_columns(data, ["repository_name", "repository_url", "open_issue_count", "closed_issue_count", "open_closed_ratio", "commit_count", "original_codebase_size", "library_codebase_size", "library_to_original_ratio", "repository_type", "primary_language", "creation_date", "stargazer_count", "license_info", "latest_release"])
+
+    # Quality Measure
+    quality_measure_thresholds = OrderedDict()
+    quality_measure_thresholds["repository_url"]  = columns["repository_url"] 
+    quality_measure_thresholds["open_closed_ratio"] = quality_measure(columns["open_closed_ratio"])
+    quality_measure_thresholds["commit_count"] = quality_measure(columns["commit_count"])
+    quality_measure_thresholds["creation_date"] = calculate_quality_measure_date(columns["creation_date"])
+    quality_measure_thresholds["stargazer_count"] = quality_measure(columns["stargazer_count"])
+    quality_measure_thresholds["latest_release"] = calculate_quality_measure_date(columns["latest_release"])
+
+    # Write to CSV
+    df = pd.DataFrame(quality_measure_thresholds)
+    df = df.rename(columns={0: 'repository_url'})
+    df = df.rename(columns={1: 'open_closed_ratio'})
+    df = df.rename(columns={2: 'commit_count'})
+    df = df.rename(columns={3: 'creation_date'})
+    df = df.rename(columns={4: 'stargazer_count'})
+    df = df.rename(columns={5: 'latest_release'})
+    df.to_csv('thresholds.csv', index=False)
+
+    # Singular Quality Measures, Write to CSV    
+    quality_measures = calculate_quality_measure("thresholds.csv")
+
+    df = pd.DataFrame(quality_measures.items(), columns=['repository_url', 'quality_measure'])
+    df.to_csv('quality_measures.csv', index=False)
+
+    # Visualize Linear Regression
+    # Ratio of Library Code : Open Closed Ratio
+    # Ratio of Library Code : Stargazer Count
+    # Ratio of Library Code : Commit Count
+    # Ratio of Library Code : Creation Date
+    # Ratio of Library Code : Latest Release
+    # Ratio of Library Code : Average Quality Measure
+    
 
 main()
